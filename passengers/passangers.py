5c96031fd5e690cb1bd23bb90a9f86cdc1b7ae9a
KostyaBobroff <k.bobrov.jr@gmail.com>
2018-03-18 00:20:03 +0300

    Add new homework

--- passengers/passangers.py
+++ passengers/passangers.py
@@ -2,38 +2,62 @@
 
 def passenger_walk(trains, passenger_name, steps):
     for train in trains:
-        for car in train['cars']:
+        for car_index, car in enumerate(train['cars']):
             if passenger_name in car['people']:
-                if 0 <= steps + train['cars'].index(car) <= len(train['cars']):
+                if 0 <= steps + car_index <= len(train['cars']):
                     car['people'].remove(passenger_name)
-                    train['cars'][train['cars'].index(car) + steps]['people'].append(passenger_name)
+                    train['cars'][car_index + steps]['people'].append(passenger_name)
                     return 1
                 else:
                     return -1
     return -1
 
 
+# def cars_switch(event, trains):
+#     taking_train = None
+#     coupling_cars = None
+#     flag1 = False
+#     flag2 = False
+#     for train in trains:
+#         if train['name'] == event['train_from']:
+#             print('this is cars ', event['cars'])
+#             coupling_cars = train['cars'][len(train) - event['cars']:]
+#             #for i in range(len(train) - event['cars'], len(train)):
+#               #  del train['cars'][i]
+#
+#             train['cars'] = train['cars'][:- event['cars']]
+#             if taking_train is not None:
+#                 # train['cars'].extend(coupling_cars)
+#                 taking_train['car'].extend(coupling_cars)
+#             flag1 = True
+#         if train['name'] == event['train_to']:
+#             taking_train = train
+#             if coupling_cars is not None:
+#                 train['cars'].extend(coupling_cars)
+#             coupling_cars = None
+#         flag2 = True
+#     if flag1 and flag2:
+#         return 1
+#     else:
+#         return -1
+
 def cars_switch(event, trains):
-    taking_train = None
-    coupling_cars = None
-    flag1 = False
-    flag2 = False
-    for train in trains:
+    first_train = None
+    second_train = None
+    for train_index, train in enumerate(trains):
+
         if train['name'] == event['train_from']:
             print('this is cars ', event['cars'])
-            coupling_cars = train['cars'][len(train) - event['cars']:]
-            for i in range(len(train) - event['cars'], len(train)):
-                del train['cars'][i]
-            if taking_train is not None:
-                train['cars'].extend(coupling_cars)
-            flag1 = True
+            first_train = train
+            first_train_index = train_index
         if train['name'] == event['train_to']:
-            taking_train = train
-            if coupling_cars is not None:
-                train['cars'].extend(coupling_cars)
-            coupling_cars = None
-        flag2 = True
-    if flag1 and flag2:
+            second_train = train
+
+    if second_train and first_train and 1 <= event['cars'] <= len(first_train['cars']):
+        coupling_cars = first_train['cars'][first_train_index + event['cars']:]
+        first_train['cars'] = first_train['cars'][:-event['cars']]
+        second_train['cars'].extend(coupling_cars)
+        print(trains)
         return 1
     else:
         return -1
