b04d5da7da0bc43de575be947a98ed285682df0a
KostyaBobroff <k.bobrov.jr@gmail.com>
2018-03-25 20:26:29 +0300

    Add regExp for ignoring www and files

--- log_parse/log_parse.py
+++ log_parse/log_parse.py
@@ -3,6 +3,34 @@ from collections import Counter
 import re
 
 
+def parse_urls(logs, urls_counter):
+    for log in logs:
+        parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log)
+        if parser:
+            urls_counter[parser.group()] += 1
+    return urls_counter
+
+
+def parse_urls_without_files(logs, urls_counter):
+    for log in logs:
+        parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*/', log)
+        if parser:
+            urls_counter[parser.group()] += 1
+    return urls_counter
+
+
+def parse_urls_without_www(logs, urls_counter):
+    for log in logs:
+        parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log)
+
+        if parser:
+            edit_parser = re.search('(?<=www.)\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*/', parser.group())
+            if edit_parser is None:
+                edit_parser = parser
+            urls_counter[edit_parser.group()] += 1
+    return urls_counter
+
+
 def parse(
         ignore_files=False,
         ignore_urls=[],
@@ -13,19 +41,19 @@ def parse(
         slow_queries=False
 ):
     f = open("log.log", "r+")
-    logs = list(f)
+    data = list(f)
     f.close()
-    # print(len(logs))
+
     # regexp for URLS
     # <схема>:[//[<логин>:<пароль>@]<хост>[:<порт>]][/]<URL‐путь>[?<параметры>][#<якорь>]
-    # ''[a-z]*:[/]{2}[\w]*:?[\w]*@*[\w]*:?[\d]*[^?\s]*'
-    urls_counter = Counter()
-    for log in logs:
-        parser = re.search('[a-z]*:[/]{2}[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log)
-        if parser is not None:
-            urls_counter[parser.group()] += 1
-        # print(parser.group())
-    return urls_counter.most_common(5)
+    # [a-z]*:[/]{2}[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*
+    # regex для урлов без файлов в листе)
+    # (?<=/)[\w]+.[\w]+$
+    # regex для урлов когда bool в листе)
+    # (?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*/
+    urls = Counter()
+    urls = parse_urls_without_www(data, urls)
 
+    return urls.most_common(5)
 
-#parse()
+# print(parse())
