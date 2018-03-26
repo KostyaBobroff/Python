5e8e33a8c66420f239571e15a0ec6afcf4e1623a
KostyaBobroff <k.bobrov.jr@gmail.com>
2018-03-26 23:41:00 +0300

    Add all functions and rewrite code

--- log_parse/log_parse.py
+++ log_parse/log_parse.py
@@ -1,6 +1,18 @@
-# -*- encoding: utf-8 -*-
-from collections import Counter
 import re
+from datetime import datetime, date, time
+from collections import Counter
+
+
+def parse_urls_without_files(logs):
+    # #string = re.sub(r"(?<=./)[-.\w]+[.]\w+(?=[?\s])","",arr[0])
+    # for log in logs:
+    #     parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log['url'])
+    #     if parser and not re.search('\w+\.\w+$',parser.group()):
+    for log in logs[:]:
+        parser = re.search(r"(?<=./)[-.\w]+[.]\w+(?=[?\s])", log)
+        if parser:
+            logs.remove(log)
+    return logs
 
 
 def parse_urls(logs, urls_counter):
@@ -11,24 +23,59 @@ def parse_urls(logs, urls_counter):
     return urls_counter
 
 
-def parse_urls_without_files(logs, urls_counter):
-    for log in logs:
-        parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*/', log)
-        if parser:
-            urls_counter[parser.group()] += 1
-    return urls_counter
-
-
-def parse_urls_without_www(logs, urls_counter):
-    for log in logs:
+def parse_ignore_urls(logs, ignore_urls):
+    for log in logs[:]:
         parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log)
+        if parser and parser.group() in ignore_urls:
+            logs.remove(log)
+    return logs
 
+
+def parse_urls_without_www(logs):
+    return [re.sub(r"(?<=://)www.", "", log) for log in logs]
+
+
+def start_at_time(logs, start):
+    for log in logs[:]:
+        parser = re.search("(?<=\[)\d{2}/\w+/\w{4}", log)
         if parser:
-            edit_parser = re.search('(?<=www.)\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*/', parser.group())
-            if edit_parser is None:
-                edit_parser = parser
-            urls_counter[edit_parser.group()] += 1
+            date_from_log = datetime.strptime(parser.group(0), '%d/%b/%Y')
+            if start > date_from_log:
+                logs.remove(log)
+    return logs
+
+
+def stop_at_time(logs, stop):
+    for log in logs[:]:
+        parser = re.search("(?<=\[)\d{2}/\w+/\w{4}", log)
+        if parser:
+            date_from_log = datetime.strptime(parser.group(0), '%d/%b/%Y')
+            if stop < date_from_log:
+                logs.remove(log)
+    return logs
+
+
+def parse_urls_slow_quire(logs, urls_counter):
+    count = Counter()
+    for log in logs:
+        parser = re.search('(?<=://).*\s([\d]+)(?=\n)', log)
+        if parser:
+            edit_parser = re.search('^[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', parser.group())
+            urls_counter[edit_parser.group()] += int(parser.group(1))
+            count[edit_parser.group()] += 1
+
+    for key, elem in urls_counter.items():
+        urls_counter[key] = elem // count[key]
     return urls_counter
+
+
+def parse_by_request_type(logs, request_type):
+    for log in logs:
+        parser = re.search('(?<=")\w+(?=.*://)', log)
+        if parser:
+            if request_type != parser.group():
+                logs.remove(log)
+    return logs
 
 
 def parse(
@@ -44,6 +91,30 @@ def parse(
     data = list(f)
     f.close()
 
+    if ignore_files:
+        data = parse_urls_without_files(data)
+    if ignore_urls:
+        data = parse_ignore_urls(data, 'www.sys.mail.ru/calendar/config/254/40263/')
+    if ignore_www:
+        data = parse_urls_without_www(data)
+    if start_at:
+        data = start_at_time(data, start_at)
+    if stop_at:
+        data = stop_at_time(data, stop_at)
+    if request_type:
+        data = parse_by_request_type(data, request_type)
+    urls = Counter()
+    if slow_queries:
+        urls = parse_urls_slow_quire(data, urls)
+    else:
+        urls = parse_urls(data, urls)
+
+        # urls = par
+    ####datetime.datetime(2005, 7, 14, 0, 0)
+    ####d.strftime("%d/%b/%Y")
+
+    # if ignore_files:
+    #     log_dict = parse_urls_without_files(data)
     # regexp for URLS
     # <схема>:[//[<логин>:<пароль>@]<хост>[:<порт>]][/]<URL‐путь>[?<параметры>][#<якорь>]
     # [a-z]*:[/]{2}[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*
@@ -51,9 +122,11 @@ def parse(
     # (?<=/)[\w]+.[\w]+$
     # regex для урлов когда bool в листе)
     # (?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*/
-    urls = Counter()
-    urls = parse_urls_without_www(data, urls)
+    #  urls = Counter()
+    #  urls = parse_urls_without_www(data,urls)
+    result = list()
+    # for i in urls.most_common(5):
+    #     result
+    result = list(map(lambda x: x[1], urls.most_common(5)))
+    return result
 
-    return urls.most_common(5)
-
-# print(parse())
