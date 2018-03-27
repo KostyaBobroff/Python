b71483565b7c1433719ec3a73d34c977eac85ed9
KostyaBobroff <k.bobrov.jr@gmail.com>
2018-03-27 15:15:58 +0300

    Remake log_parse.py

--- log_parse/log_parse.py
+++ log_parse/log_parse.py
@@ -1,77 +1,69 @@
 import re
 from datetime import datetime, date, time
-from collections import Counter
+from collections import defaultdict
 
 
-def parse_urls_without_files(logs):
-    for log in logs[:]:
-        parser = re.search(r"(?<=./)[-.\w]+[.]\w+(?=[?\s])", log)
-        if parser:
-            logs.remove(log)
-    return logs
+def parse_urls_without_files(log):
+    parser = re.search(r"(?<=./)[-.\w]+[.]\w+(?=[?\s])", log)
+    return parser
 
 
-def parse_urls(logs, urls_counter):
-    for log in logs:
-        parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log)
-        if parser:
-            urls_counter[parser.group()] += 1
-    return urls_counter
+def parse_urls(log):
+    parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log)
+    if parser:
+        return parser.group(0)
+    else:
+        return None
 
 
-def parse_ignore_urls(logs, ignore_urls):
-    for log in logs[:]:
-        parser = re.search('(?<=//)[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', log)
-        if parser and parser.group() in ignore_urls:
-            logs.remove(log)
-    return logs
+def parse_ignore_urls(log, ignore_urls):
+    if log in ignore_urls:
+        return True
+    else:
+        return False
 
 
-def parse_urls_without_www(logs):
-    return [re.sub(r"(?<=://)www.", "", log) for log in logs]
+def parse_urls_without_www(log):
+    return re.sub(r"(?<=://)www.", "", log)
 
 
-def start_at_time(logs, start):
-    for log in logs[:]:
-        parser = re.search("(?<=\[)\d{2}/\w+/\w{4}", log)
-        if parser:
-            date_from_log = datetime.strptime(parser.group(0), '%d/%b/%Y')
-            if start > date_from_log:
-                logs.remove(log)
-    return logs
+def start_at_time(log, start):
+    parser = re.search("(?<=\[)\d{2}/\w+/\w{4}\s\w+:\w+:\w+", log)
+    if parser:
+        date_from_log = datetime.strptime(parser.group(0), '%d/%b/%Y %H:%M:%S')
+        if start > date_from_log:
+            return True
+        else:
+
+            return False
 
 
-def stop_at_time(logs, stop):
-    for log in logs[:]:
-        parser = re.search("(?<=\[)\d{2}/\w+/\w{4}", log)
-        if parser:
-            date_from_log = datetime.strptime(parser.group(0), '%d/%b/%Y')
-            if stop < date_from_log:
-                logs.remove(log)
-    return logs
+def stop_at_time(log, stop):
+    parser = re.search("(?<=\[)\d{2}/\w+/\w{4}\s\w+:\w+:\w+", log)
+    if parser:
+        date_from_log = datetime.strptime(parser.group(0), '%d/%b/%Y %H:%M:%S')
+        if stop < date_from_log:
+            return True
+        else:
+            return False
 
 
-def parse_urls_slow_quire(logs, urls_counter):
-    count = Counter()
-    for log in logs:
-        parser = re.search('(?<=://).*\s([\d]+)(?=\n)', log)
-        if parser:
-            edit_parser = re.search('^[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', parser.group())
-            urls_counter[edit_parser.group()] += int(parser.group(1))
-            count[edit_parser.group()] += 1
-
-    for key, elem in urls_counter.items():
-        urls_counter[key] = elem // count[key]
-    return urls_counter
+def parse_urls_slow_quire(log):
+    parser = re.search('(?<=://).*\s([\d]+)(?=\n)', log)
+    if parser:
+        edit_parser = re.search('^[\w]*:?[\w]*@?[\w]*:?[\d]*[/]?[^?\s]*', parser.group(1))
+        return edit_parser.group()
+    else:
+        return None
 
 
-def parse_by_request_type(logs, request_type):
-    for log in logs:
-        parser = re.search('(?<=")\w+(?=.*://)', log)
-        if parser:
-            if request_type != parser.group():
-                logs.remove(log)
-    return logs
+def parse_by_request_type(log, request_type):
+    parser = re.search('(?<=")\w+(?=.*://)', log)
+    if parser:
+        if request_type != parser.group():
+            return True
+        else:
+            return False
 
 
 def parse(
@@ -83,27 +75,42 @@ def parse(
         ignore_www=False,
         slow_queries=False
 ):
-    f = open("log.log", "r+")
-    data = list(f)
-    f.close()
+    urls = defaultdict(lambda: {'querie_time': 0, 'count': 0})
+    with open('log.log') as f:
+        for line in f:
+            if ignore_files:
+                if parse_urls_without_files(line):
+                    continue
+            if ignore_urls:
+                if parse_ignore_urls(line, ignore_urls):
+                    continue
+            if ignore_www:
+                line = parse_urls_without_www(line) if parse_urls_without_www(line) else line
+            if start_at:
+                if start_at_time(line, start_at):
+                    continue
+            if stop_at:
+                if stop_at_time(line, stop_at):
+                    continue
+            if request_type:
+                if parse_by_request_type(line, request_type):
+                    continue
 
-    if ignore_files:
-        data = parse_urls_without_files(data)
-    if ignore_urls:
-        data = parse_ignore_urls(data, 'www.sys.mail.ru/calendar/config/254/40263/')
-    if ignore_www:
-        data = parse_urls_without_www(data)
-    if start_at:
-        data = start_at_time(data, start_at)
-    if stop_at:
-        data = stop_at_time(data, stop_at)
-    if request_type:
-        data = parse_by_request_type(data, request_type)
-    urls = Counter()
+            if slow_queries:
+                if parse_urls_slow_quire(line):
+                    urls[parse_urls(line)]['querie_time'] += int(parse_urls_slow_quire(line))
+                    urls[parse_urls(line)]['count'] += 1
+
+            else:
+                if parse_urls(line):
+                    urls[parse_urls(line)]['count'] += 1
+    res = list()
     if slow_queries:
-        urls = parse_urls_slow_quire(data, urls)
+        for val in urls.values():
+            res.append(val['querie_time'] // val['count'])
+        res = sorted(res, reverse=True)
     else:
-        urls = parse_urls(data, urls)
+        urls = sorted(urls.items(), key=lambda x: x[1]['count'], reverse=True)
+        res = [val[1]['count'] for val in urls]
 
-    result = list(map(lambda x: x[1], urls.most_common(5)))
-    return result
+    return res[:5]
